2615785cb29f4e0e6c31a2262eeb953c0e017c47
Rafael Santos <rafaels@gmail.com>
2020-10-20 21:07:20 -0400

    contrast and brightness maniputlations

--- cpp_prjs/003/003_image_math.cpp
+++ cpp_prjs/003/003_image_math.cpp
@@ -29,5 +29,21 @@ int main(){
     img_uint.convertTo(img_uint, CV_8UC3, 1.0/scaling_factor, -shift/scaling_factor);
     showImage("UINT image", img_uint);
 
+    // manipulating contrast of an image
+    double contrast_percent = 30.0;
+    cv::Mat img_contrast_high = img.clone();
+    img_contrast_high.convertTo(img_contrast_high, CV_64F, scaling_factor, shift); // remember the scaling factor and shift in the conversion function
+    img_contrast_high = img_contrast_high * (1 + contrast_percent/100.0);
+    showImage("High Contrast", img_contrast_high);
+
+    // manipulating brightness of an image
+    cv::Scalar brightness_factor = cv::Scalar(50.0, 50.0, 50.0);
+    cv::Mat brightness_matrix = cv::Mat(img.size().height, img.size().width, CV_8UC3, brightness_factor);
+    cv::Mat img_brightness_high = img.clone();
+    cv::add(img_brightness_high, brightness_factor, img_brightness_high);
+    // show the channel splitting option as well
+
+    showImage("Brighter Image", img_brightness_high);
+    
     return 0;
 }
