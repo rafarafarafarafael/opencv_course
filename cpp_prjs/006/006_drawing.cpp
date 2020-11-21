435bbec7c76204b24e3783cd7e54a1831fececa2
Rafael Santos <rafaels@gmail.com>
2020-11-21 12:54:31 -0500

    only thing left is drawing text now

--- cpp_prjs/006/006_drawing.cpp
+++ cpp_prjs/006/006_drawing.cpp
@@ -22,14 +22,23 @@ int main(){
     cv::line(img, pt0, pt1, GREEN, 2, cv::LINE_AA);
     showImage("Boy with line", img);
 
-    // drawing a circle
+    // drawing an ellipse
     const cv::Point ctr0 = cv::Point(240, 125), ax0 = cv::Point(100, 50), ax1 = cv::Point(50, 100);
     const cv::Scalar RED = cv::Scalar(0, 0, 255), BLUE = cv::Scalar(255, 0, 0);
     cv::ellipse(img, ctr0, ax0, 0, 0, 360, RED, 2, cv::LINE_AA);
     cv::ellipse(img, ctr0, ax1, 0, 0, 360, BLUE, 2, cv::LINE_AA);
     showImage("Boy with line and ellipses", img);
 
+    // drawing a circle
+    int rad0 = 150;
+    cv::circle(img, ctr0, rad0, GREEN, 2, cv::LINE_AA);
+    showImage("Boy lines circle and ellipses", img);
+
     // drawing a rectangle
+    const cv::Point pt2 = cv::Point(170, 50), pt3 = cv::Point(310, 200);
+    const cv::Scalar MAGENTA = cv::Scalar(255, 0, 255);
+    cv::rectangle(img, pt2, pt3, MAGENTA, 3, cv::LINE_8);
+    showImage("All with a rectangle now", img);
 
     return 0;
 }
