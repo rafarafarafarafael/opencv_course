f4f86481d763be80ff158ed2960a3b5b705e27f0
Rafael Santos <rafaels@gmail.com>
2020-11-23 12:00:41 -0500

    started week2 chapter on video capture and display

---
+++ cpp_prjs/007/007_vide_capture.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+void showImage(const std::string & win, cv::Mat & img){
+    cv::namedWindow(win);
+    cv::imshow(win, img);
+    cv::waitKey();
+    cv::destroyAllWindows();
+}
+
+int main(){
+    cv::VideoCapture cap;
+    cv::Mat frame;
+
+    
+
+
+    return 0;
+}
