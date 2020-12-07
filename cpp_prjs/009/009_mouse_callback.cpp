1f247ff2886b648871ea023284eb831e6e662908
Rafael Santos <rafaels@gmail.com>
2020-12-07 05:49:05 -0500

    started mouse callback example

---
+++ cpp_prjs/009/009_mouse_callback.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+
+void showImage(const std::string & win, cv::Mat & img){
+    cv::namedWindow(win);
+    cv::imshow(win, img);
+    cv::waitKey();
+    cv::destroyAllWindows();
+}
+
+int main(){
+
+    return 0;
+}
