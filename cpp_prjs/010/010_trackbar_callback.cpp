106570b555ecb3673bc15d635b5c509649fa15d8
Rafael Santos <rafaels@gmail.com>
2020-12-14 06:14:12 -0500

    scaled down implemented

--- cpp_prjs/010/010_trackbar_callback.cpp
+++ cpp_prjs/010/010_trackbar_callback.cpp
@@ -5,28 +5,33 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
+// global variables
 int max_scale_up = 100;
 int scale_factor = 1;
 int scale_type = 0;
 int max_type = 1;
-
 const std::string window_name = "Resize Image";
 const std::string trackbar_value = "Scale";
 const std::string trackbar_type = "Type: \n 0: Scale Up \n 1: Scale Down";
 
+// image to be used, for some reason I couldn't declare this variable as local to main()
 cv::Mat img = cv::imread(".\\images\\truth.png");
 
+// callback definition
 void scaleImage(int, void*);
 
 int main(){
     cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
 
+    // trackbar creation and callback assignment
     cv::createTrackbar(trackbar_value, window_name, &scale_factor, max_scale_up, scaleImage);
     cv::createTrackbar(trackbar_type, window_name, &scale_type, max_type, scaleImage);
 
+    // not sure why this one...
     scaleImage(25, 0);
 
     while(true){
+        // loop until ESC pressed
         int c = cv::waitKey(20);
         if(static_cast<char>(c) == 27){
             break;
@@ -36,14 +41,28 @@ int main(){
     return 0;
 }
 
+// callback implementation
 void scaleImage(int, void*){
-    double scale_factor_double = 1 + scale_factor/100.0;
+    // scale factor declared and initialized
+    double scale_factor_double = 1.0;
+
+    if(scale_type == 1){
+        scale_factor_double += scale_factor/100.0; // scale up
+    } else {
+        scale_factor_double -= scale_factor/100.0; // scale down
+    }
+
+    // clamp scale in extreme cases
     if(scale_factor_double == 0){
-        scale_factor_double = 1.0;
+        if(scale_type == 1){
+            scale_factor_double = 1.0;
+        } else {
+            scale_factor_double = 0.01;
+        }
     }
     
+    // creating scaled up version and display
     cv::Mat scaled_image;
-
     cv::resize(img, scaled_image, cv::Size(), scale_factor_double, scale_factor_double, cv::INTER_LINEAR);
     cv::imshow(window_name, scaled_image);
 }
