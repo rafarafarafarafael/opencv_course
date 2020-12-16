7c3f2f23ca54081ffb5461611414b5a991aec490
Rafael Santos <rafaels@gmail.com>
2020-12-16 09:32:02 -0500

    made changes to include external utility functions

--- cpp_prjs/011/011_thresholding.cpp
+++ cpp_prjs/011/011_thresholding.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <string>
 #include <opencv2/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include "..\lib\show_image.hpp" // external utility function
 
+int main(){
+    const std::string win_name = "Window";
+    const std::string img_path = ".\\images\\boy.jpg";
+
+    cv::Mat img = cv::imread(img_path);
+
+    showImage(win_name, img);
+
+    return 0;
+}
