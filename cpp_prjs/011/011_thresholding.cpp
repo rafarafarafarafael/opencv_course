2890b0a64a140e3492ad4fe3d4a7bf0768c5a7bb
Rafael Santos <rafaels@gmail.com>
2020-12-28 08:23:15 -0500

    finished basics of thresholding

--- cpp_prjs/011/011_thresholding.cpp
+++ cpp_prjs/011/011_thresholding.cpp
@@ -7,18 +7,39 @@
 #include "..\lib\show_image.hpp" // external utility function
 
 int main(){
-    const std::string win_name = "Window";
+    const std::string win_name = "Original";
     const std::string img_path = ".\\images\\threshold.png";
 
     cv::Mat img = cv::imread(img_path);
     showImage(win_name, img);
 
     int thresh = 100;
-    int thresh_max = 200;
+    int thresh_max = 150;
 
-    auto thresh1 = img.clone();
-    cv::threshold(img, thresh1, thresh, thresh_max, cv::THRESH_BINARY);
-    showImage(win_name, thresh1);
+    // basic binary threshold
+    auto dst_bin = img.clone();
+    cv::threshold(img, dst_bin, thresh, thresh_max, cv::THRESH_BINARY);
+    showImage("Binary Threshold", dst_bin);
+
+    // inverse binary threshold
+    auto dst_inv_bin = img.clone();
+    cv::threshold(img, dst_inv_bin, thresh, thresh_max, cv::THRESH_BINARY_INV);
+    showImage("Inverse Binary Threshold", dst_inv_bin);
+
+    // truncate threshold
+    auto dst_trunc = img.clone();
+    cv::threshold(img, dst_trunc, thresh, thresh_max, cv::THRESH_TRUNC);
+    showImage("Truncate Threshold", dst_trunc);
+
+    // threshold to zero
+    auto dst_to_zero = img.clone();
+    cv::threshold(img, dst_to_zero, thresh, thresh_max, cv::THRESH_TOZERO);
+    showImage("Threshold To Zero", dst_to_zero);
+
+    // inverted threshold to zero
+    auto dst_inv_to_zero = img.clone();
+    cv::threshold(img, dst_inv_to_zero, thresh, thresh_max, cv::THRESH_TOZERO_INV);
+    showImage("Inverse Threshold To Zero", dst_inv_to_zero);
 
     return 0;
 }
