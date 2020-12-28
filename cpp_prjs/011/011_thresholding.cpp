a58b0f0c5212821a4f1d96a4296a74e7aa1cfbdf
Rafael Santos <rafaels@gmail.com>
2020-12-28 08:00:49 -0500

    basic binary threshold using opencv functions, would like to try implementing my own at some point

--- cpp_prjs/011/011_thresholding.cpp
+++ cpp_prjs/011/011_thresholding.cpp
@@ -8,11 +8,17 @@
 
 int main(){
     const std::string win_name = "Window";
-    const std::string img_path = ".\\images\\boy.jpg";
+    const std::string img_path = ".\\images\\threshold.png";
 
     cv::Mat img = cv::imread(img_path);
-
     showImage(win_name, img);
+
+    int thresh = 100;
+    int thresh_max = 200;
+
+    auto thresh1 = img.clone();
+    cv::threshold(img, thresh1, thresh, thresh_max, cv::THRESH_BINARY);
+    showImage(win_name, thresh1);
 
     return 0;
 }
