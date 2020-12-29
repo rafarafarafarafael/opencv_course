722776bf458fec343c17a4ca114a5a8a026e2c18
Rafael Santos <rafaels@gmail.com>
2020-12-29 06:32:45 -0500

    started morphological operations

---
+++ cpp_prjs/012/012_morphological_ops.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include "..\lib\show_image.hpp" // external utility function
+
+int main(){
+
+    return 0;
+}
