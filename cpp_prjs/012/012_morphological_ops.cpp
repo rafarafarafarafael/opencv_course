f1d544778bf1d1c4cac8b4f98d633801047ac417
Rafael Santos <rafaels@gmail.com>
2020-12-29 06:41:09 -0500

    basic loadign code for 012

--- cpp_prjs/012/012_morphological_ops.cpp
+++ cpp_prjs/012/012_morphological_ops.cpp
@@ -6,6 +6,10 @@
 #include "..\lib\show_image.hpp" // external utility function
 
 int main(){
+    const std::string img_path = ".\\images\\dilation_example.jpg";
+    auto img = cv::imread(img_path);
 
+    showImage("Original", img);
+    
     return 0;
 }
