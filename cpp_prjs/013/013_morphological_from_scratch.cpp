71fa4ee568b74cd6a13e70ef67ec8e9dfb49ddf6
Rafael Santos <rafaels@gmail.com>
2021-01-04 11:13:03 -0500

    working on auto copying the needed dll

--- cpp_prjs/013/013_morphological_from_scratch.cpp
+++ cpp_prjs/013/013_morphological_from_scratch.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include <time.h>
 #include "..\lib\show_image.hpp" // external utility function
 
