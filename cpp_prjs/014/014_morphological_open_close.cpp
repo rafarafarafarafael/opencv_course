5ed0a08f0ec8ce23df297cde073faa5300782fc1
Rafael Santos <rafaels@gmail.com>
2021-01-05 14:06:17 -0500

    finished morphological opening and closing

--- cpp_prjs/014/014_morphological_open_close.cpp
+++ cpp_prjs/014/014_morphological_open_close.cpp
@@ -21,7 +21,7 @@ int main(){
     // method 1: erosion step followed by dilation
     cv::Mat img_eroded = img_opening.clone();
     cv::erode(img_opening, img_eroded, element, cv::Point(-1, -1), 1);
-    showImage("Eroded", img_eroded);
+    //showImage("Eroded", img_eroded);
 
     cv::Mat img_eroded_dilated = img_eroded.clone();
     cv::dilate(img_eroded, img_eroded_dilated, element, cv::Point(-1, -1), 1);
@@ -33,6 +33,22 @@ int main(){
     cv::Mat img_morph_opening;
     cv::morphologyEx(img_opening, img_morph_opening, cv::MORPH_OPEN, element, cv::Point(-1, -1), 3);
     showImage("Morph Opening", img_morph_opening);
+
+    // morphological closing
+    // method 1: dialation followedby erosion
+    cv::Mat img_dilated = img_closing.clone();
+    element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(2 * kernel_size + 1, 2 * kernel_size + 1), cv::Point(kernel_size, kernel_size));
+    cv::dilate(img_closing, img_dilated, element, cv::Point(-1, -1), 1);
+    cv::Mat img_dilated_eroded = img_dilated.clone();
+    cv::erode(img_dilated, img_dilated_eroded, element, cv::Point(-1, -1), 1);
+    showImage("Dilated + Eroded = Close", img_dilated_eroded);
+
+    // method 2: morphologyEx function
+    int closing_size = 10;
+    element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(2 * closing_size + 1, 2 * closing_size + 1), cv::Point(closing_size, closing_size));
+    cv::Mat img_morph_closing = img_closing.clone();
+    cv::morphologyEx(img_closing, img_morph_closing, cv::MORPH_CLOSE, element);
+    showImage("Morph Closing", img_morph_closing);
 
     return 0;
 }
