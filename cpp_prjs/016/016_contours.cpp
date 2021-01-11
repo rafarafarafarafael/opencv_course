7392b2b3b6a05e60f9a489265fccaa6593216b45
Rafael Santos <rafaels@gmail.com>
2021-01-10 23:50:22 -0500

    started on contours

---
+++ cpp_prjs/016/016_contours.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <opencv2\core.hpp>
+#include <opencv2\opencv.hpp>
+#include <opencv2\highgui.hpp>
+#include <opencv2\imgproc.hpp>
+#include "..\lib\show_image.hpp"
+
+
+int main(){
+
+    return 0;
+}
