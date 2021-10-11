d2a6a40e7a6961bce23a10e705206030bebe46d4
Rafael Santos <rafaels@gmail.com>
2021-10-11 15:02:09 -0400

    fixed example project to use external utility funcitons

--- cpp_prjs/000/000_base_project.cpp
+++ cpp_prjs/000/000_base_project.cpp
@@ -2,16 +2,15 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include "..\lib\show_image.hpp" // external utility function
 
 int main(){
     std::string imagePath = ".\\images\\boy.jpg";
     cv::Mat testImage = cv::imread(imagePath);
 
-    const std::string winname = "test";
+    const std::string winname = "Image";
 
-    cv::namedWindow(winname);
-    cv::imshow(winname, testImage);
-    cv::waitKey();
+    showImage(winname, testImage);  
 
     return 0;
 }
