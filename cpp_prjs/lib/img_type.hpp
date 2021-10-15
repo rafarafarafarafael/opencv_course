78a670395eb0fe1e8369da59833505962d73cf75
Rafael Santos <rafaels@gmail.com>
2021-10-15 10:21:12 -0400

    added new util function to output image data type

---
+++ cpp_prjs/lib/img_type.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+std::string type2str(int);
