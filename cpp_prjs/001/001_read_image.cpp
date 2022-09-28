7d3da19204f1b91bf8f0c52f02aa099ba168acfa
Rafael Santos <rafaels@gmail.com>
2022-09-27 22:46:42 -0400

    fixed project to compile using utility functions

--- cpp_prjs/001/001_read_image.cpp
+++ cpp_prjs/001/001_read_image.cpp
@@ -2,6 +2,8 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include "..\lib\img_type.hpp" // external utility function
+#include "..\lib\show_image.hpp" // external utility function
 
 int main(){
     std::string imagePath = ".\\images\\boy.jpg";
@@ -9,9 +11,13 @@ int main(){
 
     const std::string winname = "test";
 
-    cv::namedWindow(winname);
-    cv::imshow(winname, testImage);
-    cv::waitKey();
+    showImage(winname, testImage);
 
+    imagePath = ".\\images\\number_zero.jpg";
+    cv::Mat zero = cv::imread(imagePath, cv::IMREAD_GRAYSCALE);
+    std::cout << zero << std::endl;
+    std::cout << "Data type: " << type2str(zero.type()) << std::endl;
+    std::cout << "Image dimensions: " << zero.size();
+    
     return 0;
 }
