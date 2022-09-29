9753dfcde8433ddb3eccb83a9c67d94efce5d3b4
Rafael Santos <rafaels@gmail.com>
2022-09-29 05:56:30 -0400

    Added some pixel reading and setting code

--- cpp_prjs/001/001_read_image.cpp
+++ cpp_prjs/001/001_read_image.cpp
@@ -6,18 +6,27 @@
 #include "..\lib\show_image.hpp" // external utility function
 
 int main(){
-    std::string imagePath = ".\\images\\boy.jpg";
-    cv::Mat testImage = cv::imread(imagePath);
+    std::string image_path = ".\\images\\boy.jpg";
+    cv::Mat test_image = cv::imread(image_path);
 
-    const std::string winname = "test";
+    const std::string win_name = "test";
 
-    showImage(winname, testImage);
+    showImage(win_name, test_image);
 
-    imagePath = ".\\images\\number_zero.jpg";
-    cv::Mat zero = cv::imread(imagePath, cv::IMREAD_GRAYSCALE);
+    image_path = ".\\images\\number_zero.jpg";
+    cv::Mat zero = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
     std::cout << zero << std::endl;
     std::cout << "Data type: " << type2str(zero.type()) << std::endl;
     std::cout << "Image dimensions: " << zero.size();
     
+    //pick up one pixel and inspect it
+    int pixel_value = zero.at<uchar>(6, 5);
+    std::cout << "Pixel value is: " << pixel_value << std::endl;
+
+    //set a new value for the pixel and print the matrix
+    zero.at<uchar>(6, 5) = 255;
+    std::cout << zero << std::endl;
+    showImage(win_name, zero);
+
     return 0;
 }
